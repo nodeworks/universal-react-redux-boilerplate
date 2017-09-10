fc543281937cbdc698013b4627a004495cc64289
Rob Lee <rob@nodeworks.com>
2017-09-09 23:00:57 -0400

    opsworks update

--- .circleci/opsworks.sh
+++ .circleci/opsworks.sh
@@ -1,2 +1,6 @@
 #!/usr/bin/env bash
-aws opsworks --region "us-east-1" create-deployment --stack-id $1 --app-id $2 --command "{\"Name\":\"deploy\"}";
+if [ -n "$1" ] then
+  aws opsworks --region "us-east-1" create-deployment --stack-id $1 --app-id $2 --command "{\"Name\":\"deploy\"}";
+else
+  echo "The environment variables have not been setup for AWS OpsWorks!"
+fi
